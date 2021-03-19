86bb2cd0abf7edf5607944dc2f7d571fa2e3c8f0
sunnivmr <sunnivmr@stud.ntnu.no>
2021-03-19 13:06:03 +0100

    #53 Fetch Risvolland data instead of example data

--- src/api/DataFetching.js
+++ src/api/DataFetching.js
@@ -2,25 +2,34 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function DataFetching() {
-  const [post, setPost] = useState({});
+  const [dataInfo, setDataInfo] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+    console.log(dataInfo);
+  };
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`http://ibmrisvol.ibm.ntnu.no/data/info?id=${idFromButtonClick}`)
       .then((res) => {
         console.log(res);
-        setPost(res.data);
+        setDataInfo(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [id]);
+  }, [idFromButtonClick]);
 
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
-      <div>{post.title}</div>
+      <button type="button" onClick={handleClick}>
+        Fetch post
+      </button>
+      <div>{dataInfo.description}</div>
     </div>
   );
 }
