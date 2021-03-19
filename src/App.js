a2804b65664214e74ce615e3e78c1ece66dba10a
sunnivmr <sunnivmr@stud.ntnu.no>
2021-03-19 11:01:42 +0100

    #53 Installed axios, fetched data from example API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import DataFetching from "./api/DataFetching";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DataFetching />
     </div>
   );
 }
