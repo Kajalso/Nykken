a23e43328e00f721d6dbea68f619b77f8d3d7723
sunnivmr <sunnivmr@stud.ntnu.no>
2021-04-12 12:52:24 +0200

    #66 Changed font and organized some css-files

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,11 +3,20 @@ import React from "react";
 import { SensorChart } from "./SensorChart/SensorChart";
 import { Button } from "../Button/Button";
 
+import "./dashboard.scss";
+
 export const Dashboard = () => {
   return (
     <>
-      <h1>Sensor measurements</h1>
-      <Button text="Edit dashboard" />
+      <div className="title-buttons">
+        <h2 className="title">Sensor measurements</h2>
+        <div className="buttons">
+          <Button text="Edit dashboard" />
+          <Button text="Create section" />
+          <Button text="Create custom chart" />
+        </div>
+      </div>
+
       <div className="sensor-grid">
         <SensorChart />
       </div>
