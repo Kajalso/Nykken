04e27e3530b2a72851f190d2c7b49cf1e0c84bdc
sunnivmr <sunnivmr@stud.ntnu.no>
2021-04-13 10:04:53 +0200

    #64 Added logic for opening and closing modal, added modals for Create section and Create custom chart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,13 +8,38 @@ import "./dashboard.scss";
 
 export const Dashboard = () => {
   const [editModalIsOpen, setEditModalIsOpen] = useState(false);
+  const [groupModalIsOpen, setGroupModalIsOpen] = useState(false);
+  const [customChartModalIsOpen, setCustomChartModalIsOpen] = useState(false);
 
   const EditModal = ({ isOpen }) => {
     return (
-      <Modal isOpen={isOpen}>
+      <Modal isOpen={isOpen} onRequestClose={() => setEditModalIsOpen(false)}>
         <h4>Edit dashboard</h4>
         <p>Select which sensors you want to see on the dashboard</p>
         <button onClick={() => setEditModalIsOpen(false)}>Close</button>
+      </Modal>
+    );
+  };
+
+  const GroupModal = ({ isOpen }) => {
+    return (
+      <Modal isOpen={isOpen} onRequestClose={() => setGroupModalIsOpen(false)}>
+        <h4>Edit dashboard</h4>
+        <p>Select which sensors you want to see on the dashboard</p>
+        <button onClick={() => setGroupModalIsOpen(false)}>Close</button>
+      </Modal>
+    );
+  };
+
+  const CustomChartModal = ({ isOpen }) => {
+    return (
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={() => setCustomChartModalIsOpen(false)}
+      >
+        <h4>Edit dashboard</h4>
+        <p>Select which sensors you want to see on the dashboard</p>
+        <button onClick={() => setCustomChartModalIsOpen(false)}>Close</button>
       </Modal>
     );
   };
@@ -23,8 +48,23 @@ export const Dashboard = () => {
     <>
       <div className="title-buttons">
         <h2 className="title">Sensor measurements</h2>
-        <div className="buttons"></div>
+        <div className="buttons">
+          <Button
+            text="Edit dashboard"
+            onClick={() => setEditModalIsOpen(true)}
+          />
+          <Button
+            text="Create section"
+            onClick={() => setGroupModalIsOpen(true)}
+          />
+          <Button
+            text="Create custom chart"
+            onClick={() => setCustomChartModalIsOpen(true)}
+          />
+        </div>
         <EditModal isOpen={editModalIsOpen} />
+        <GroupModal isOpen={groupModalIsOpen} />
+        <CustomChartModal isOpen={customChartModalIsOpen} />
       </div>
 
       <div className="sensor-grid">
