5a18b2177e7616bc70295e21c2dab04d8b78050d
Kaja Solberg <kajasolberg@Kajas-MacBook-Pro.local>
2021-04-16 15:28:21 +0200

    Created LineChart.test.js and added some tests

--- src/__tests__/LineChart.test.js
+++ src/__tests__/LineChart.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import { LineChart } from '../components/SensorChart/LineChart/LineChart';
 
 
-//Surpressing warning as suggested by:
+//Suppressing warning as suggested by:
 const originalError = global.console.error;
 beforeAll(() => {
 	global.console.error = jest.fn((...args) => {
@@ -18,12 +18,63 @@ afterAll(() => {
   	global.console.error.mockRestore();
 });
 
+//Providing props
+const data = [
+    {0: {measurement: "17", time_stamp_utc: "2020-08-08T00:00:00Z" }},
+    {1: {measurement: "17", time_stamp_utc: "2020-08-08T00:01:00Z" }}
+];
+
+const data2 = [
+    {0: {measurement: "17", time_stamp_utc: "2020-09-08T00:00:00Z" }},
+    {1: {measurement: "17", time_stamp_utc: "2020-09-08T00:01:00Z" }}
+];
+
+const dI1 = {
+    data_identifier: 1,
+    description: "Temperature",
+}
+
+const dI2 = {
+    data_identifier: 2,
+    description: "Air Temperature",
+}
+
 describe('renders without crashing and takes snapshot', () => {
     test('renders correctly', () => {
-        render(< LineChart />);
+        render(< LineChart data={data} dataInfo={dI1} />);
     });
     test('takes snapshot', () => {
-        const { component } = render(< LineChart />);
+        const { component } = render(< LineChart data={data} dataInfo={dI1} />);
         expect(component).toMatchSnapshot();
     })
+
 })
+
+describe('displays correct elements', () => {
+    test('display correct sensor name (Temperature)', () => {
+        const { getByText } = render(< LineChart data={data} dataInfo={dI1} />);
+        getByText('Temperature');
+    })
+
+    test('display correct sensor name (Air Temperature)', () => {
+        const { getByText } = render(< LineChart data={data} dataInfo={dI2} />);
+        getByText('Air Temperature');
+    })
+
+    test('display correct sensor ID', () => {
+        const { getByText } = render(< LineChart data={data} dataInfo={dI1} />);
+        getByText('Sensor ID: 1');
+    });
+
+})
+describe('Converts date to text correctly', () => {
+    test('08082020 ', () => {
+        const { queryByText } = render(< LineChart data={data} dataInfo={dI1} />);
+        queryByText(/Saturday 08 August 2020/i);
+    })
+    test('090820', () => {
+        const { queryByText } = render(< LineChart data={data2} dataInfo={dI1} />);
+        queryByText(/Sunday 09 August 2020/i);
+    })
+})
+   
