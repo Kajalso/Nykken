09c9aa0ee1875c038a985a163adb37bf54398da3
sunnivmr <sunnivmr@stud.ntnu.no>
2021-04-30 13:51:34 +0200

    #76 Restructured code and folders to make it cleaner and clearer

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,31 +1,19 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import Modal from "react-modal";
-import { SensorGroups } from "../SensorMeasurements/SensorGroups";
+import { SensorGroups } from "../SensorGroups/SensorGroups";
 
 import { SensorMeasurements } from "../SensorMeasurements/SensorMeasurements";
-
-import { GroupsContext } from '..//../context/GroupsContext';
 
 import "./dashboard.scss";
 
 if (process.env.NODE_ENV !== "test") Modal.setAppElement("#root");
 
 export const Dashboard = () => {
-  const { groups } = useContext(GroupsContext);
   return (
     <div className="dashboard">
       <SensorMeasurements />
-      <h2 className="title">My Groups</h2>
-      {groups.length === 0 && <p>No groups to show</p>}
-      {groups &&
-      groups.map((group) => (
-        <SensorGroups
-          groupName={group.groupName}
-          group={group}
-          key={group.id}
-        />  
-      ))}
+      <SensorGroups />
     </div>
   );
 };
--- src/components/SensorGroups/GroupChart.js
+++ src/components/SensorGroups/GroupChart.js
@@ -1,29 +1,24 @@
 import React from "react";
 
-import { LineChart } from "./LineChart/LineChart";
-import { BarChart } from "./BarChart/BarChart";
+import { LineChart } from "../SensorChart/LineChart/LineChart";
+import { BarChart } from "../SensorChart/BarChart/BarChart";
 
 import { useSensorData } from "../../api/useSensorData";
 
-
-import "./sensorChart.scss";
-
-
 const barChartIDs = [5, 8, 10, 12];
 
-
-export const ChartGroup = ({ 
-    dataInfo,
-    startDateTime,
-    endDateTime,
-    granularity
- }) => {
-    const id = dataInfo.data_identifier;
+export const GroupChart = ({
+  dataInfo,
+  startDateTime,
+  endDateTime,
+  granularity,
+}) => {
+  const id = dataInfo.data_identifier;
 
   // Fetch sensor data and data info
   const sensorData = useSensorData(id, startDateTime, endDateTime, granularity);
- 
- return (
+
+  return (
     <div className="sensor-chart">
       <h3 className="section-title">{dataInfo.title}</h3>
 
@@ -33,16 +28,10 @@ export const ChartGroup = ({
       {sensorData && sensorData[0] && (
         <>
           {barChartIDs.includes(id) && (
-            <BarChart
-              data={sensorData}
-              dataInfo={dataInfo}
-            />
+            <BarChart data={sensorData} dataInfo={dataInfo} />
           )}
           {!barChartIDs.includes(id) && (
-            <LineChart
-              data={sensorData}
-              dataInfo={dataInfo}
-            />
+            <LineChart data={sensorData} dataInfo={dataInfo} />
           )}
         </>
       )}
